02be5910dd5d094f4f8d81d4f47d98e81b9caade
utsav8196-del <utsav8196@gmail.com>
2026-04-14 11:11:21 +0530

    prit

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+
 
 const ROOM_RATES = {
   Standard: 2000,
